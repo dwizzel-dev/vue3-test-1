51dbc2695f240d717f85d7b3235e5a9c6826cb04
dwizzel-dev <dwizzel.dev@gmail.com>
2018-07-08 15:21:09 -0400

    crlf fix vscode fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    // fix linebreak problems on windows and linux and with vscode
+    'linebreak-style': 0,
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
